f08b8907d3a7c20447c9514e0ae39ff670ad58c3
Andi Muhammad Ichsan Jalaluddin <andi.jalaluddin@multipolar.com>
2025-04-28 11:10:13 +0700

    update

--- app.py
+++ app.py
@@ -4,23 +4,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")  # Set di Cloud Run atau .env
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/geocode', methods=['POST'])
-def geocode():
-    location = request.form.get('location')
-    url = f'https://maps.googleapis.com/maps/api/geocode/json?address={location}&key={GOOGLE_API_KEY}'
-    response = requests.get(url)
-    result = response.json()
-    if result['status'] == 'OK':
-        latlng = result['results'][0]['geometry']['location']
-        return render_template('index.html', lat=latlng['lat'], lng=latlng['lng'], location=location)
-    else:
-        return render_template('index.html', error="Lokasi tidak ditemukan.")
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
